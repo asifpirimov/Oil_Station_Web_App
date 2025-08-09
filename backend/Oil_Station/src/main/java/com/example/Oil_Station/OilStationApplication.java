05e9c7dadd7e2396d18d7dd069f97ad759300b9b
Asif Pirimzade <asifpirimov99@gmail.com>
2025-08-09 16:23:40 +0400

    Added Spring Boot backend source files

---
+++ backend/Oil_Station/src/main/java/com/example/Oil_Station/OilStationApplication.java
@@ -0,0 +1,13 @@
+package com.example.Oil_Station;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OilStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OilStationApplication.class, args);
+	}
+
+}
