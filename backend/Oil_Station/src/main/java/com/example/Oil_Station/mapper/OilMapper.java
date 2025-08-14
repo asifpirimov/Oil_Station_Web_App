e5fd49eb3d85c02ed0f35b3ce4f381f7b7f5b7f5
Asif Pirimzade <asifpirimov99@gmail.com>
2025-08-15 00:56:21 +0400

    Simple Modifications

--- backend/Oil_Station/src/main/java/com/example/Oil_Station/mapper/OilMapper.java
+++ backend/Oil_Station/src/main/java/com/example/Oil_Station/mapper/OilMapper.java
@@ -1,8 +1,11 @@
 package com.example.Oil_Station.mapper;
 
+import org.springframework.context.annotation.Configuration;
+
 import com.example.Oil_Station.dto.OilDTO;
 import com.example.Oil_Station.entity.Oil;
 
+@Configuration
 public class OilMapper {
     public OilDTO toDTO(Oil oil) {
         return OilDTO.builder()
